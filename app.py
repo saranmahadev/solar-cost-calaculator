c6f1f999023316de4f7715c889bb2c9adc93a691
Saran Mahadev <sarandevnet@gmail.com>
2021-03-05 19:19:24 +0530

    Test App

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-from flask import Flask,render_template,request
-app = Flask(__name__,template_folder="templates")
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def hello():
-    return render_template('index.html')
-
-@app.route('/data',methods=['POST'])
-def func_name():    
-    temp = ""    
-    actual_data = {}
-
-    for data in request.form:
-        
-        # Splits the Name of the Data Sent
-        dataset = data.split('_')
-        
-        print(data)        
-
-
-
-        
-    return actual_data
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run()
